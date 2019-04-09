67fe1bc3febce7aa238c5b47f56a05d5c01c1f2f
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-04-09 09:10:32 +0800

    initial commit

---
+++ tastefunction.py
@@ -0,0 +1,22 @@
+# Read through the code below. Even if you don't understand it, try to make 
+# a guess about what it does. What do you think will happen when you press 
+# "Test Run"? Once you have a prediction, press "Test Run" and compare what 
+# happens to what you predicted.
+
+def say_hello():
+    return "Hello!"
+
+print say_hello()
+
+
+def sum(firstnumber, secondnumber):
+    return firstnumber + secondnumber
+
+
+print sum(5,7)
+
+# say_hello is a function. Or, as Dave would call it, a procedure.
+# This procedure isn't particularly interesting because it only does
+# one thing. 
+
+# Continue to the next example to see a more interesting version of say_hello.
